6d351987beac7471bed3115261f0a9d5f038e138
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-03-29 00:05:10 +0530

    input set in details page

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import Header from '../../components/header'
-import { MainBackground } from "../../assets"
-import SearchInput from '../../components/searchInput'
+import React from "react";
+import Header from "../../components/header";
+import { MainBackground } from "../../assets";
+import SearchInput from "../../components/searchInput";
 const index = () => {
   return (
     <div
-     style={{
+      style={{
         backgroundImage: `url(${MainBackground})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-      }}>
-    <Header/>
-    <div className="w-full flex justify-center">
-    <div className="max-w-[1300px] w-full flex-col   flex justify-center">
-    <div className="w-full flex items-center   justify-center">
-            <div className="flex gap-3   w-[80%] mt-15 mb-15 flex-col ">
-              <SearchInput /> 
+      }}
+    >
+      <Header />
+      <div className="w-full flex justify-center">
+        <div className="max-w-[1300px] w-full flex-col   flex justify-center">
+          {/* ***********search input **************** */}
+          <div className="w-full flex items-center   justify-center">
+            <div className="flex gap-3   w-[60%] mt-15 mb-15 flex-col ">
+              <SearchInput />
             </div>
           </div>
-</div>
-</div>
+          {/* *************details components******************** */}
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
