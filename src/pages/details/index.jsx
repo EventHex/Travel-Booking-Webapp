607f3f479f1ca2352ddabb9bab12e16232a732a2
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-03-28 23:57:59 +0530

    working on deatil page

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Header from '../../components/header'
+import { MainBackground } from "../../assets"
+import SearchInput from '../../components/searchInput'
 const index = () => {
   return (
-    <div>
+    <div
+     style={{
+        backgroundImage: `url(${MainBackground})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}>
     <Header/>
+    <div className="w-full flex justify-center">
+    <div className="max-w-[1300px] w-full flex-col   flex justify-center">
+    <div className="w-full flex items-center   justify-center">
+            <div className="flex gap-3   w-[80%] mt-15 mb-15 flex-col ">
+              <SearchInput /> 
+            </div>
+          </div>
+</div>
+</div>
     </div>
   )
 }
