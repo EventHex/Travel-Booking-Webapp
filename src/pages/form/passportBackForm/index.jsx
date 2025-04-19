681cbea271979b11ae3d4a3d9fa108b64b6254bd
Salah-Zain <salahzain36@gmail.com>
2025-04-19 19:05:31 +0530

    Enhance TravelVisaBooking form to support image uploads and improve data handling. Added state management for front and back passport images, integrated file upload functionality, and implemented a new API call to fetch traveler information after submission. Updated FrontPassportForm and BackPassportForm components to accept image props.

--- src/pages/form/passportBackForm/index.jsx
+++ src/pages/form/passportBackForm/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import Input from "../../../components/input";
 
-export const BackPassportForm = ({ formData, setFormData }) => {
+export const BackPassportForm = ({ formData, setFormData, backImageUrl, setBackImageUrl }) => {
   const [isProcessing, setIsProcessing] = useState(false);
   const [processingError, setProcessingError] = useState(null);
-  const [backImageUrl, setBackImageUrl] = useState(null);
+  // const [backImageUrl, setBackImageUrl] = useState(null);
   const [previewUrl, setPreviewUrl] = useState(null);
 
   const handleFileUpload = async (event) => {
